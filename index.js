6f67e894929cada43558d2922be2d07c0649bdad
DylanToporek <dylantoporek@gmail.com>
2021-09-09 16:41:47 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    const cards = [];
+    for (let i = 0; i < names.length; i++) {
+    cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+    return cards;
+}
+writeCards(["Lisa", "Kaitlin", "Jan"], "surprise");
+
+
+function countDown(x) {
+    while (x >= 0) {
+        console.log(x);
+        x--
+    }
+    
+    
+}
+countDown(10);
